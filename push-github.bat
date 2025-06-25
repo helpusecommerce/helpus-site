0d9ff208a1d1100c02713901e976947112257fdc
HelpUSA <helpus.ecommerce@gmail.com>
2025-06-25 10:46:09 -0300

    Atualizacao automatica

--- push-github.bat
+++ push-github.bat
@@ -3,9 +3,9 @@ cd /d "D:\Documents\OneDrive\Wagner\EUA\Empresa\helpus llc\Desenvolvimento\GitHu
 echo Adicionando arquivos modificados...
 git add .
 echo Criando commit...
-git commit -m "Atualização automática"
+git commit -m "Atualizacao automatica"
 echo Enviando para o GitHub...
 git push origin main
 echo.
-echo ✅ Código enviado com sucesso!
+echo ✅ Codigo enviado com sucesso!
 pause
