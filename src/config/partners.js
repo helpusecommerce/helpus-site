7bc495ba4d307fcb718cab586e5b5ad1b9e82b00
HelpUSA <helpus.ecommerce@gmail.com>
2025-09-15 22:47:04 -0300

    Atualiza├º├úo autom├ítica

--- src/config/partners.js
+++ src/config/partners.js
@@ -2,13 +2,16 @@
 const BASE = 'helpusbr.com';
 
 export const partners = {
-  wagnerdriver:      `https://wagnerdriver.${BASE}`,
-  cgdetails:         `https://cgdetails.${BASE}`,
-  bluebox:           `https://bluebox.${BASE}`,
-  publicarte:        `https://publicarte.${BASE}`,
-  memoriaviva:       `https://memoriaviva.${BASE}`,
-  tuliobicicletas:   `https://tuliobicicletas.${BASE}`,
-  waleska:           `https://waleska.${BASE}`,
-  katiaxavier:       `https://katiaxavier.${BASE}`,
-  marciotopbarber:   `https://marciotopbarber.${BASE}`,
+  wagnerdriver:        `https://wagnerdriver.${BASE}`,
+  cgdetails:           `https://cgdetails.${BASE}`,
+  bluebox:             `https://bluebox.${BASE}`,
+  publicarte:          `https://publicarte.${BASE}`,
+  memoriaviva:         `https://memoriaviva.${BASE}`,
+  tuliobicicletas:     `https://tuliobicicletas.${BASE}`,
+  waleska:             `https://waleska.${BASE}`,
+  katiaxavier:         `https://katiaxavier.${BASE}`,
+  marciotopbarber:     `https://marciotopbarber.${BASE}`,
+  escolaestacaomusical:`https://escolaestacaomusical.${BASE}`,
 };
+
+
