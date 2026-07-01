d53483be6931f1de7cee35fdc60a15862fb627d2
Wagner Medeiros dos Santos <wagner.redes@gmail.com>
2026-07-01 18:15:10 -0300

    Fix useLocation import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // arquivo: src/App.js
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CookieConsent from './components/CookieConsent';
